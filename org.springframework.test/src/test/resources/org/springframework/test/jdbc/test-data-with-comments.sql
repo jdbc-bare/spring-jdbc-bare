0a583462eb595aa079ccf29d1ef5a66f27a3aacf
pdudits <pato@dudits.net>
2013-01-21 22:17:30 +0100

    Removed all other modules

--- org.springframework.test/src/test/resources/org/springframework/test/jdbc/test-data-with-comments.sql
+++
@@ -1,6 +0,0 @@
-insert into customer (id, name)
-values	(1, 'Rod; Johnson'), (2, 'Adrian Collier');
--- This is also a comment.
-insert into orders(id, order_date, customer_id)
-values (1, '2008-01-02', 2);
-insert into orders(id, order_date, customer_id) values (1, '2008-01-02', 2);
